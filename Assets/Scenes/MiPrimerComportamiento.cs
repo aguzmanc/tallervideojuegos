ac70a5c66da18916df34562c8ce7a8c4791bb512
aguzmanc <spacerockganimedes@gmail.com>
2020-08-21 09:53:29 -0400

    Primer commit

---
+++ Assets/Scenes/MiPrimerComportamiento.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiPrimerComportamiento : MonoBehaviour
+{
+    int edad;
+    
+
+    // Start es llamado antes del primer Update del primer cuadro (frame)
+    void Start()
+    {
+        name = "Rogelio";
+        edad = 0;
+    }
+
+    // Update es llamado una vez cada frame
+    void Update()
+    {
+        edad = edad+1;
+
+        name = "Rogelio: " + edad;
+    }
+}
