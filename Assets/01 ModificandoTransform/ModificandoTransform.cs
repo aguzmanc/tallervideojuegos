036b790cc1bd9efcf61fb04f7d5606d9dc17c8db
aguzmanc <spacerockganimedes@gmail.com>
2020-08-21 12:05:34 -0400

    Clase Sprites

---
+++ Assets/01 ModificandoTransform/ModificandoTransform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModificandoTransform : MonoBehaviour
+{
+    public int parametroY;
+    public float velocidad;
+
+
+
+    public float rotacion;
+
+
+    void Start()
+    {
+        rotacion = 0;
+
+        transform.position = new Vector3(1, parametroY, 3);
+
+        GetComponent<Rigidbody>().velocity = new Vector3(0,0,5);
+        
+    }
+
+
+    void Update()
+    {
+        //transform.rotation = Quaternion.Euler(rotacion,50,30);
+        //rotacion = rotacion + velocidad;
+        //      3 + 7.8
+        //          10.8
+        
+    }
+}
---
+++ Assets/MovimientoMario.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoMario : MonoBehaviour
+{
+    public SpriteRenderer rend;
+
+    public Sprite cuadro1;
+    public Sprite cuadro2;
+
+    public int n;
+
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+
+        rend.sprite = cuadro1;
+
+        n = 0;
+    }
+
+    void Update()
+    {
+        if(n==0){
+            rend.sprite = cuadro2;
+            n=1;
+        } else {
+            rend.sprite = cuadro1;
+            n = 0;
+        }
+        
+        
+    }
+}
