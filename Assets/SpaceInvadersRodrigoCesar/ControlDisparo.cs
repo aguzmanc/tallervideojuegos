5bfdc608a671787a507f8937fdee0b647a40fae8
RodrigoYucraV <tucht6060@gmail.com>
2020-10-07 11:55:17 -0400

    Disparo limitado - Colisiona con Enemy tag

---
+++ Assets/SpaceInvadersRodrigoCesar/ControlDisparo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDisparo : MonoBehaviour
+{
+    private Transform proyectil;
+    public float speed;
+    void Start()
+    {
+        proyectil = GetComponent<Transform>();
+    }
+    private void FixedUpdate()
+    {
+        proyectil.position += Vector3.up * speed;
+        if (proyectil.position.y >= 100)
+            Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D otro)
+    {
+        if (otro.tag == "Enemy")
+        {
+            Destroy(otro.gameObject);
+            Destroy(gameObject);
+
+        }
+        else if (otro.tag == "Base")
+            Destroy(gameObject);
+    }
+
+}
