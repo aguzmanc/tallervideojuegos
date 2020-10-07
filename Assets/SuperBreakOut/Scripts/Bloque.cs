f40f685c6ac049990af4985eca6254122f8f270f
Andresguz <andresguzguzman15@gmail.com>
2020-10-07 10:59:53 -0400

    prueba1 destroybloque

---
+++ Assets/SuperBreakOut/Scripts/Bloque.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameObject.SetActive(false); 
+    }
+    
+}
