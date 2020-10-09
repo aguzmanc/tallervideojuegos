2aa780a4f3aebed83d02825c296456e38bb41b33
JorgeAlvarez26 <gylar26@gmail.com>
2020-10-09 10:49:55 -0400

    Añadido movimiento y direccion del Jugador

---
+++ Assets/Metal Slug 3/Scripts/Player/GunController.cs
@@ -0,0 +1,32 @@
+﻿using MetalSlug;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    PlayerMovement player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.GetComponentInParent<PlayerMovement>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //Idle
+        transform.rotation = player.lastDirection;
+        
+        //LookUp
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+        
+
+       
+
+    }
+}
