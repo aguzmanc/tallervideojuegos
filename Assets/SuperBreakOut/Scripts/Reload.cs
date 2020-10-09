d315170e7d5a94f99c7d165ef9d3dd08be22b6f6
GNubiaDB <davidperez96ch@gmail.com>
2020-10-09 12:09:25 -0400

    reload

---
+++ Assets/SuperBreakOut/Scripts/Reload.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class Reload : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+            //Application.LoadLevel is depreciated; use the scene manager function
+            //that you get from 'using SceneManager'
+            //You can see there are a few overloads for this function; you can pass a string (name) or number for the scene.
+            SceneManager.LoadScene("Super");
+        }
+    }
+}
