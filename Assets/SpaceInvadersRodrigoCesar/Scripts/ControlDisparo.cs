4e33f57ff9988ad3a21386a3341edaabaa601f83
TaiyaNovato <Taiya99@hotmail.com>
2020-10-13 21:26:06 -0400

    Nave Movil y Armada

---
+++ Assets/SpaceInvadersRodrigoCesar/Scripts/ControlDisparo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDisparo : MonoBehaviour
+{
+    private Transform proyectil;
+    public float speed;
+   
+    void Start()
+    {
+        proyectil = GetComponent<Transform>();
+    }
+    void FixedUpdate()
+    {
+        proyectil.position += Vector3.up * speed;
+        if (proyectil.position.y >= 10)
+            Destroy(gameObject);
+    }
+    void OnTriggerEnter2D(Collider2D otro)
+    {
+        if (otro.tag == "Enemigo")
+        {
+            Destroy(otro.gameObject);
+            Destroy(gameObject);
+        }
+        else if (otro.tag == "Base")
+            Destroy(gameObject);
+    }
+
+}
---
+++ Assets/SpaceInvadersRodrigoCesar/Scripts/baseHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class baseHealth : MonoBehaviour
+{
+    public float health = 2;
+
+    void Update ()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
