c422555c3262917447184c09a0e573a479fab76e
RodrigoYucraV <tucht6060@gmail.com>
2020-10-14 12:02:06 -0400

    Alien Movimiento comportamineto y Score-Game Over

---
+++ Assets/SpaceInvadersRodrigoCesar/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static float scoreTabla = 0;
+    private Text scoreText;
+   
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    
+    void Update()
+    {
+        scoreText.text = "Score: " + scoreTabla;
+    }
+}
