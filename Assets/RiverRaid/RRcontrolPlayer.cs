c1c611b610d4f86dafea2a691a3430c16db74acb
GusarDurak <gusar25pormal@gmail.com>
2020-10-16 11:04:08 -0400

    scripts RR

---
+++ Assets/RiverRaid/RRcontrolPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RRcontrolPlayer : MonoBehaviour
+{
+    Animator animator;
+    
+    void Start()
+    {
+        animator= GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetBool ("izquierda", true);
+        }
+        else
+        {
+            animator.SetBool ("izquierda", false);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetBool ("derecha", true);
+        }
+        else
+        {
+            animator.SetBool ("derecha", false);
+        }
+    }
+}
---
+++ Assets/RiverRaid/rrIAEnemyRiver.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rrIAEnemyRiver : MonoBehaviour
+{
+    // Start is called before the first frame update
+        float rrAleatorio;
+        float rrcomienzaPatrulla;
+        float rrcontadorpatrulla;
+        float rrEnMovimiento = 0;
+    void Start()
+    {
+        rrAleatorio = Random.Range(1.0f, 10.0f);
+        rrcontadorpatrulla = 0.0f;
+        rrcomienzaPatrulla = rrAleatorio;
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rrcontadorpatrulla = rrcontadorpatrulla + 0.01f;
+        transform.Translate(rrEnMovimiento, 0, 0);
+        if (rrcontadorpatrulla > rrcomienzaPatrulla)
+        {
+            rrEnMovimiento = Time.deltaTime;
+            rrcontadorpatrulla = -10.0f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D otro)
+    {
+        if (otro.tag == "rrborde")
+        {
+            rrEnMovimiento = -1*Time.deltaTime;
+            Debug.Log("detectado");
+        }
+    }
+
+}
