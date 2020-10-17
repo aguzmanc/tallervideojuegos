48fd78d09fd0750d0d66d7c930da5f69ffaae780
RodrigoYucraV <tucht6060@gmail.com>
2020-10-17 16:23:42 -0400

    Escena Funcional Basico

---
+++ Assets/SpaceInvadersRodrigoCesar/Scripts/EnemigoProyectil.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoProyectil : MonoBehaviour
+{
+    private Transform balaenemiga;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        balaenemiga = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        balaenemiga.position += Vector3.up * -speed;
+        if (balaenemiga.position.y <= -10)
+            Destroy(balaenemiga.gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag =="Player")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            GameOver.isPlayerDead = true;
+        }
+        else if (other.tag == "Base")
+        {
+            //GameObject 
+        }
+    }
+}
---
+++ Assets/SpaceInvadersRodrigoCesar/Scripts/controllEnemigo.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controllEnemigo : MonoBehaviour
+{
+    private Transform enemyHolder;
+    public float speed;
+    public GameObject shot;
+    public GameObject shot1;
+    public GameObject shot2;
+    public Text winText;
+    public float Cdisparo = 0.997f;
+    void Start()
+    {
+        winText.enabled = false;
+        InvokeRepeating("MoveEnemy", 0.1f, 0.3f);
+        enemyHolder = GetComponent<Transform>();
+
+    }
+    void MoveEnemy()
+
+    {
+        enemyHolder.position += Vector3.right * speed;
+        foreach(Transform enemy in enemyHolder)
+        {
+            if (enemy.position.x< -70 || enemy.position.x > 70)
+            {
+                speed = -speed;
+                enemyHolder.position += Vector3.down * 0.5f;
+                return;
+            }
+            if (enemy.position.y<= -10)
+            {
+                GameOver.isPlayerDead = true;
+                Time.timeScale = 0;
+            }
+            if (enemyHolder.childCount == 1)
+            {
+                CancelInvoke();
+                InvokeRepeating("MoveEnemy", 0.1f, 0.25f);
+            }
+            if (enemyHolder.childCount == 0)
+            {
+                winText.enabled = true;  
+            }
+        }
+
+        //Movimiento pausado de los enemigos
+    }
+}
