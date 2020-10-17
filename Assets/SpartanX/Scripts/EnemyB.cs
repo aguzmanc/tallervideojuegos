f6a4c95ae22cfbe43d11097847152099ccaa0112
Dattronk <dattronk@electronico.com>
2020-10-17 16:37:33 -0400

    Escena B, jugador programado con scripts

---
+++ Assets/SpartanX/Scripts/EnemyB.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class EnemyB : MonoBehaviour
+{
+    //Movimiento
+    public float speed;
+    float direccion;
+
+    //Sprites
+    public Sprite attacking;
+    public Sprite[] walksprites;
+    public Sprite[] attacksprites;
+    public Sprite deadsprite;
+    SpriteRenderer sp;
+
+    //Walkin Animation
+    int walkanimcount;
+    public float animspeed;
+    float nextanimtime;
+
+    //States
+    bool walkanim = true;
+    bool attackanim = false;
+    bool canwalk = true;
+    bool dead = false;
+    public bool atacando = false;
+
+    //Attack
+    public float damage = 0.001f;
+
+    //Components
+    Rigidbody2D rb;
+    AudioSource AS;
+    public AudioClip hit;
+    EnemyB enem;
+    Jugador jscript;
+    
+
+
+
+    void Start()
+    {   //Referencias a componentes
+        sp = gameObject.GetComponent<SpriteRenderer>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        AS= gameObject.GetComponent<AudioSource>();
+
+    }
+    void FixedUpdate()
+
+    {   
+        if (canwalk)
+        {
+            //Traslacion
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+
+        //Animacion de caminar
+        if (walkanim)
+        {
+            if (Time.time > nextanimtime)
+            {
+                sp.sprite = walksprites[walkanimcount];
+                if (walkanimcount == 0) { walkanimcount = 1; } else { walkanimcount = 0; }
+                nextanimtime = Time.time + animspeed * Time.deltaTime;
+            }
+        }
+
+        //Animacion de Ataque
+        if (attackanim)
+        {
+            if (Time.time > nextanimtime)
+            {
+                sp.sprite = attacksprites[walkanimcount];
+                if (walkanimcount == 0) { walkanimcount = 1; } else { walkanimcount = 0; }
+                nextanimtime = Time.time + animspeed * Time.deltaTime;
+            }
+        }
+        //Colision logica
+        if (atacando)
+        {
+            if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X))
+            {
+                if ((transform.rotation.y == 0 && direccion<0) ||(transform.rotation.y < 0 && direccion ==0))
+                {
+                    AS.PlayOneShot(hit);
+                    dead = true;
+                    atacando = false;
+                }
+                
+            }
+        }
+
+        if (dead)
+        {
+            rb.simulated = false;
+            sp.sprite = deadsprite;
+           
+            transform.Translate(-speed * Time.deltaTime, -speed * Time.deltaTime, 0, Space.Self);
+            Destroy(this.gameObject, 2f);
+           
+        }
+
+
+
+
+
+    }
+    //Evento de colision
+    private void OnTriggerEnter2D(Collider2D other)
+    {   //Colision con otro enemigo
+        if (other.CompareTag("Enemigo"))
+        {
+            enem = other.gameObject.GetComponent<EnemyB>();
+            if (enem.atacando)
+            {
+                canwalk = false;
+                attackanim = false;
+                sp.sprite = attacking;
+                atacando = true;
+            }
+           
+        }
+        //Colision con el jugador
+        if (other.CompareTag("golpe"))
+        {
+            jscript = other.gameObject.GetComponentInParent<Jugador>();
+            if (jscript.kicking || jscript.punching)
+            {
+                dead = true;
+                atacando = false;
+                AS.PlayOneShot(hit);
+            }
+        }
+         if (other.CompareTag("cuerpo")) 
+            { 
+                canwalk = false;
+                attackanim = false;
+                atacando = true;
+                sp.sprite = attacking;
+            }
+           
+        
+
+        //Preparacion para el ataque
+        if (other.CompareTag("proximidad"))
+        {
+            walkanim = false;
+            attackanim = true;
+            
+        }
+
+        
+       
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag ("Enemigo"))
+        {
+            attackanim = true;
+            canwalk = true;
+            atacando = false;
+        }
+
+       
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag ("cuerpo"))
+        {
+            direccion = other.gameObject.transform.rotation.y;
+            atacando = true;
+        }
+    }
+}
