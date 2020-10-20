13a8db1e227a571261f2653eb0f8d6437b7ecdfe
TaiyaNovato <Taiya99@hotmail.com>
2020-10-20 17:23:56 -0400

    Escena funcionando

---
+++ Assets/SpaceInvadersRodrigoCesar/Scripts/SoundManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    // Sostiene la instancia individual del SoundManager que 
+    // puede acceder de cualquier script
+    public static SoundManager Instance = null;
+
+    // Todos los efectos de sonidos en el juego
+    // Todos  publicos para prepararlos en el inspector
+
+    public AudioClip alienBuzz1;
+    public AudioClip alienBuzz2;
+    public AudioClip alienDies;
+    public AudioClip bulletFire;
+    public AudioClip shipExplosion;
+
+    private AudioSource soundEffectAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        AudioSource theSource = GetComponent<AudioSource>();
+        soundEffectAudio = theSource;
+    }
+    // otros GameObjects pueden llamar para reproducir estos sonidos
+    
+    public void PlayOneShot(AudioClip clip)
+    {
+        soundEffectAudio.PlayOneShot(clip);
+    }
+
+
+}
