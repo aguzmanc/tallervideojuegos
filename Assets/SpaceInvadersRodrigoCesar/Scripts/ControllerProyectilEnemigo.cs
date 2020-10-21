6503c3d0dbc694358f7a8adb6992e9ed0ddf2df2
TaiyaNovato <Taiya99@hotmail.com>
2020-10-21 00:17:21 -0400

    Hecho

---
+++ Assets/SpaceInvadersRodrigoCesar/Scripts/ControllerProyectilEnemigo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerProyectilEnemigo : MonoBehaviour
+{
+    private Transform proyectil;
+    public float speed;
+
+    void Start()
+    {
+        proyectil = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        proyectil.position += Vector3.up * -speed;
+        if (proyectil.position.y <= -10)
+            Destroy(proyectil.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            GameOver.isPlayerDead = true;
+        }
+        //else if (other.tag == "Base")
+        //{
+        //    GameObject playerBase = other.gameObject;
+        //    BaseHealth baseHealth = playerBase.GetComponent<BaseHealth>();
+        //    baseHealth.health -= 1;
+        //    Destroy(gameObject);
+        //}
+    }
+}
---
+++ Assets/SpaceInvadersRodrigoCesar/Scripts/EnemyController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class EnemyController : MonoBehaviour
+{
+    private Transform enemyHolder;
+    public float speed;
+
+    public GameObject proyectilEnemigo;
+    public Text winText;
+    public float fireRate = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.enabled = false;
+        InvokeRepeating("MoveEnemy", 0.1f, 0.3f);
+        enemyHolder = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        Instantiate(proyectilEnemigo, transform.position, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void MoveEnemy()
+    {
+        enemyHolder.position += Vector3.right * speed;
+
+        foreach (Transform Enemigo in enemyHolder)
+        {
+            if (Enemigo.position.x < -10.5 || Enemigo.position.x > 10.5)
+            {
+                speed = -speed;
+                enemyHolder.position += Vector3.down * 0.5f;
+                return;
+            }
+
+            if (Random.value > fireRate)
+            {
+                Instantiate(proyectilEnemigo, Enemigo.position, Enemigo.rotation);
+            }
+
+            if (Enemigo.position.y <= -4)
+            {
+                GameOver.isPlayerDead = true;
+                Time.timeScale = 0;
+            }
+        }
+
+        if (enemyHolder.childCount == 1)
+        {
+            CancelInvoke();
+            InvokeRepeating("MoveEnemy", 0.1f, 0.25f);
+        }
+
+        if (enemyHolder.childCount == 0)
+        {
+            winText.enabled = true;
+        }
+    }
+}
