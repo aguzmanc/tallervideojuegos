a8a2de39d96a6090642499c88c14da6dc69e048b
Andresisky <andrex_mi@hotmail.com>
2020-12-02 15:30:45 -0400

    Movimiento y Disparo

---
+++ Assets/Andres-Cesar-Rodrigo/Scripts/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject arrowPrefab;
+
+    public float arrowForce = 20f;
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject arrow = Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * arrowForce, ForceMode2D.Impulse);
+    }
+}
