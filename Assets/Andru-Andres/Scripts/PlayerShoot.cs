83116c4fe44df14a2f501426f8b87ab3b94cd358
AndruxKilller7 <cristianandruortegavargas@hotmail.com>
2020-12-02 12:07:35 -0400

    Disparo Prueba

---
+++ Assets/Andru-Andres/Scripts/PlayerShoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float velocidadBala;
+    public GameObject Bala;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Bala.transform.Translate(Vector3.forward * velocidadBala * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            
+            Instantiate(Bala,transform.parent);
+            
+        }
+    }
+}
