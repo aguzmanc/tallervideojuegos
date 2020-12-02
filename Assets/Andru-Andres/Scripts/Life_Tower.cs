fa841c6238acfcff0319b5d0befc2aa8b39624c0
Andresguz <andresguzguzman15@gmail.com>
2020-12-02 11:02:59 -0400

    torre

--- Assets/Andru-Andres/Scripts/Life_Tower.cs
+++ Assets/Andru-Andres/Scripts/Life_Tower.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Life_Tower : MonoBehaviour
 {
-    
+    public float vida = 150;
+    public float vidmax = 150;
+    public Image barraDeVida;
     void Start()
     {
         
@@ -12,6 +15,7 @@ public class Life_Tower : MonoBehaviour
 
     void Update()
     {
-        
+        vida = Mathf.Clamp(vida, 0, vidmax);
+        barraDeVida.fillAmount = vida / vidmax;
     }
 }
