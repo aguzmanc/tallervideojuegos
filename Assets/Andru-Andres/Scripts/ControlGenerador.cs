54d8456e370838f206e6407fec3546086060149f
Andresguz <andresguzguzman15@gmail.com>
2020-12-03 14:13:55 -0400

    Enemigos

---
+++ Assets/Andru-Andres/Scripts/ControlGenerador.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlGenerador : MonoBehaviour
+{
+    private float rangoGeneracion = 25f;
+    private float rangoGeneracionZ = 15f;
+    public GameObject prefabEnemigo;
+    public int numeroOleada = 2;
+    public float TimeInicio = 2f;
+    public float intervaloGeneracion = 6f;
+    public float numEnemigos = 3f;
+    private float cont;
+    void Start()
+    {
+        InvokeRepeating("GeneradorEnemigos", TimeInicio, intervaloGeneracion);
+      
+      //  Debug.Log("contadir" + cont);
+    }
+
+
+    void Update()
+    {
+
+    }
+    void GeneradorEnemigos()
+    {
+        if (cont == 5)
+        {
+            numEnemigos = 0;
+        }
+        else
+        {
+            for (int i = 0; i < numEnemigos; i++)
+            {
+               
+                Instantiate(prefabEnemigo, DamePosicionGeneracion(), prefabEnemigo.transform.rotation);
+                cont++;
+            }
+        }
+
+       
+    }
+
+    Vector3 DamePosicionGeneracion()
+    {
+        float posXGeneracion = Random.Range(-rangoGeneracion, rangoGeneracion);
+        float posZGeneracion = Random.Range(-rangoGeneracionZ, rangoGeneracionZ);
+
+
+        Vector3 posAleatoria = new Vector3(posXGeneracion, 0, posZGeneracion);
+        return posAleatoria;
+    }
+}
---
+++ Assets/Andru-Andres/Scripts/Enemigo_Dongnus.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo_Dongnus : MonoBehaviour
+{
+    //variable para la vision velocidad
+    public float speed;
+    public float vidionRadio;
+    GameObject player; //para guardar jugador
+    Vector3 initialposition;//posicion inicial
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        initialposition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 target = initialposition;
+
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist < vidionRadio) target = player.transform.position;
+
+        float fixedSpeed = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
+
+        Debug.DrawLine(transform.position, target, Color.green);
+
+
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, vidionRadio);
+    }
+}
