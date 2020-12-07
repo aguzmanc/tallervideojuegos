765d554e3d01b2639bf193205344706fcd0e7fcc
JorgeAlvarez26 <gylar26@gmail.com>
2020-12-07 14:01:57 -0400

    movimiento y camara y set up de los sprites

---
+++ Assets/Alvarez-Portugal/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlvarezPortugal
+{
+
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;
+        [SerializeField] private float scaleFactor;
+        [SerializeField] private float speedFollow;
+        Camera myCam;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            myCam = GetComponent<Camera>();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            myCam.orthographicSize = (Screen.height / 100f) / scaleFactor;
+
+            if (target)
+            {
+                transform.position = Vector3.Lerp(transform.position, target.position,
+                                        speedFollow) + new Vector3(0f, 0f, -10f);
+
+            }
+        }
+    }
+}
---
+++ Assets/Alvarez-Portugal/Scripts/Player/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlvarezPortugal 
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private bool isMoving;
+
+        [SerializeField] private Rigidbody2D rb;
+       
+
+        Vector2 movement;
+        
+
+        private SpriteRenderer spriteRenderer;
+
+        private float timer;
+
+        private void Start()
+        {
+            spriteRenderer = this.GetComponent<SpriteRenderer>();
+            rb = this.GetComponent<Rigidbody2D>();
+        }
+
+        
+        void Update()
+        {
+            
+
+            //Input
+
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+
+            if (movement.x == -1f)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
+
+        }
+
+        private void FixedUpdate()
+        {
+            //Movement
+
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+            
+        }
+
+       
+    }
+}
