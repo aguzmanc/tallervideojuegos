8caa930654d8ebc20741f29a03fcb27c1f8d3599
Andresisky <andrex_mi@hotmail.com>
2020-12-11 10:46:04 -0400

    spawner

---
+++ Assets/Andres-Cesar-Rodrigo/Scripts/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float spawnTime = 2f;
+    public float spawnDealy = 3f;
+
+    public GameObject enemyPrefab;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", spawnDealy, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        Instantiate(enemyPrefab, transform.position, transform.rotation);
+    }
+}
