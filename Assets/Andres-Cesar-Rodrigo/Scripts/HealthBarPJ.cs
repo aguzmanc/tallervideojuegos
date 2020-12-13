738a412ace040ea00bafb861340fb6788839967c
RodrigoYucraV <tucht6060@gmail.com>
2020-12-13 11:55:43 -0400

    Barra y Sistema de daño

---
+++ Assets/Andres-Cesar-Rodrigo/Scripts/HealthBarPJ.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarPJ : MonoBehaviour
+{
+    public Image vida;
+    
+    float hp, maxHp = 100f;
+    void Start()
+
+    {
+        hp = maxHp;
+        
+    }
+    public void Taking (float amount)
+    {
+        hp = Mathf.Clamp(hp - amount, 0f, maxHp);
+        vida.transform.localScale = new Vector2(hp / maxHp, 1);
+    }
+}
--- Assets/Andres-Cesar-Rodrigo/Scripts/PlayerHP.cs
+++ Assets/Andres-Cesar-Rodrigo/Scripts/PlayerHP.cs
@@ -1,41 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
-    public int health = 100;
+    public float health;
+    public float maxHp = 100f;
+    public bool inmortal = false;
     public float invincibleTimeAfterHurt = 2f;
+    private GameObject healthbar;
+    HealthBarPJ barravida;
+    
 
     private AnimController anim;
-
-    public void DamagePlayer(int damage)
+    private void Start()
     {
+        healthbar = GameObject.FindGameObjectWithTag("HpYilda");
+        barravida = healthbar.GetComponent<HealthBarPJ>();
+        
+    }
+    public void DamagePlayer(float damage)
+    {
+        barravida.Taking(damage);
+        if (inmortal) return;
         health -= damage;
-
-        if (health <= 0)
-        {
-            Die();
-        }
-        /*else
-        {
-            anim.TriggerHurt(invincibleTimeAfterHurt);
-        }*/
-
+        StartCoroutine(TiempoInmortal());
+        
+      
     }
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void Curar (float vida)
     {
-        EnemyDongus enemy1 = collision.collider.GetComponent<EnemyDongus>();
-        if(enemy1 != null)
-        {
-            DamagePlayer(enemy1.enemyDamage);
-        }
+        health += vida;
     }
-
     void Die()
     {
         Destroy(gameObject);
     }
+    private void Update()
+    {
+        if (health > maxHp)
+            health = maxHp;
+        if (health <= 0)
+            Die();
+        //BarraUI
+        
+    }
+    IEnumerator TiempoInmortal()
+    {
+        inmortal = true;
+        yield return new WaitForSeconds(invincibleTimeAfterHurt);
+        inmortal = false;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        EnemyDongus enemy1 = collision.collider.GetComponent<EnemyDongus>();
+
+        if (enemy1 != null)
+        {
+            DamagePlayer(enemy1.enemyDamage);
+            
+        }
+    }
+
+
+
 }
