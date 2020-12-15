567f33f788065648eff5b556bf58c69f0cc65104
Nataly981 <maizares1010@gmail.com>
2020-12-15 16:55:16 -0400

    Scripts de los enemigos , modificaciones de los sprites

---
+++ Assets/Proyecto_Final/Scripts/enemigoAtaca.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemigoAtaca : MonoBehaviour
+{
+    //public 
+    Transform player;
+    Vector2 positionPlayer;
+    public float velocidad=0.005f;
+
+    void Start()
+    {
+        //solo busca por nombre de GameObject
+        //player = GameObject.Find("Player").transform;
+        player = GameObject.FindWithTag("Player").transform;
+        positionPlayer = new Vector2(player.position.x,player.position.y);       
+    }
+
+
+    void Update(){
+        positionPlayer = new Vector2(player.position.x,player.position.y);
+        transform.position = Vector2.Lerp(transform.position, positionPlayer,velocidad);
+    }
+    
+}
---
+++ Assets/Proyecto_Final/Scripts/enemigosCadaT.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemigosCadaT : MonoBehaviour
+{
+    public Transform prefab, player;
+    public float minRadio = 5f;
+    public float maxRadio = 10f;
+    
+    public bool stopEnemies = false;
+    public float segundos = 1;
+
+    
+    void Start()
+    {
+        //Start the coroutine we define below named ExampleCoroutine.
+        if(!stopEnemies){
+        StartCoroutine(ExampleCoroutine());
+        }
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+
+        while (!stopEnemies)
+        {
+            //Print the time of when the function is first called.
+            Debug.Log("Started Coroutine at timestamp : " + Time.time);
+            //isntanciar enemigos
+            //float angle = Mathf.Round(Time.time) * Mathf.PI * 2f / (Mathf.Round(Time.time)+10);
+            float rangoRadio = Random.Range(minRadio, maxRadio);
+            Vector2 newPos = new Vector2((Mathf.Cos(Random.Range(-360,360)) * rangoRadio) + player.position.x, (Mathf.Sin(Random.Range(-360,360)) * rangoRadio) + player.position.y);
+            Debug.Log(newPos);
+            //GameObject go = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), newPos, Quaternion.identity);
+
+            Instantiate(prefab, newPos, Quaternion.identity);
+
+            //yield on a new YieldInstruction that waits for 5 seconds.
+            yield return new WaitForSeconds(segundos);
+        }
+
+
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+
+    
+
+}
